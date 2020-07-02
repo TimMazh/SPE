2064ab7d7fc00f2af5bacb5b93d973df62df7e83
TimMazhari <tim.mazhari.work@gmail.com>
2020-07-02 21:26:31 +0200

    Page almost done (english text tbd)
    wip

--- Main/javascript/scroll.js
+++ Main/javascript/scroll.js
@@ -1,16 +1,38 @@
 $(document).ready(function(){
     $(window).scroll(function(event){
         if($(window).width() >= 1775){
-            console.log("333")
-            if($(this).scrollTop() < $(window).height()*2.5){
-                
+            if($(this).scrollTop() < $(window).height()*2.5){ 
                 $('#homeText').css({"opacity" : "0"});
-
             }
             else{
                 $("#homeText").css({"opacity" : "1"});
 
             }
+            
+            if($(this).scrollTop() < $(window).height()*4.5){ 
+                $('#immobilienText').css({"opacity" : "0"});
+            }
+            else{
+                $("#immobilienText").css({"opacity" : "1"});
+
+            }
+
+            if($(this).scrollTop() < $(window).height()*7.5){ 
+                $('#aboutUsText').css({"opacity" : "0"});
+            }
+            else{
+                $("#aboutUsText").css({"opacity" : "1"});
+
+            }
+
+            if($(this).scrollTop() < $(window).height()*9.5){ 
+                $('#contactText').css({"opacity" : "0"});
+            }
+            else{
+                $("#contactText").css({"opacity" : "1"});
+
+            }
+
         } else{
             if($(this).scrollTop() < $(window).height()*3.5){
                 
@@ -19,6 +41,30 @@ $(document).ready(function(){
             }
             else{
                 $("#homeText").css({"opacity" : "1"});
+
+            }
+
+            if($(this).scrollTop() < $(window).height()*5.5){ 
+                $('#immobilienText').css({"opacity" : "0"});
+            }
+            else{
+                $("#immobilienText").css({"opacity" : "1"});
+
+            }
+
+            if($(this).scrollTop() < $(window).height()*8.3){ 
+                $('#aboutUsText').css({"opacity" : "0"});
+            }
+            else{
+                $("#aboutUsText").css({"opacity" : "1"});
+
+            }
+
+            if($(this).scrollTop() < $(window).height()*10.3){ 
+                $('#contactText').css({"opacity" : "0"});
+            }
+            else{
+                $("#contactText").css({"opacity" : "1"});
 
             }
         }
